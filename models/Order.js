c64ac1a9f4b15b538a9e6c58bac0bb77cfda11f2
sevik2302 <sevik2302@gmail.com>
2026-04-05 02:09:18 +0300

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,1 +1,11 @@
-a
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  orderId: String,
+  amount: Number,
+  status: String,
+  receiptSent: Boolean,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Order", orderSchema);
