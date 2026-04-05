377070f80003bbbbd23f4eae75401a5d3da08fa7
sevik2302 <sevik2302@gmail.com>
2026-04-05 20:03:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -184,3 +184,8 @@ app.get("/admin", async (req, res) => {
   res.status(500).send("Ошибка");
 }
 });
+const PORT = process.env.PORT;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
+});
